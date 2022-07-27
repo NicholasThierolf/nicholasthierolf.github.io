69443d64cdac316c0ba23afd9306632aaa5ec4e8
NicholasThierol <nicholasthierolf@gmail.com>
2022-07-28 01:23:35 +0200

    1.0.0

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const content = shuffle([
   ["Wie man 8 Stunden schläft", "Einschlafen", "8 Stunden später aufwachen"],
-  ["Wie man 9 Stunden schläft", "Einschlafen", "9 Stunden späer aufwachen"],
+  ["Wie man 9 Stunden schläft", "Einschlafen", "9 Stunden später aufwachen"],
+  [
+    "Wie man keine Panik hat",
+    "Realisieren, dass fast alles im Leben im großen und ganzen meaningless ist, und dadurch die Insignifikanz der aktuellen Situation begreifen",
+    "Keine Panik mehr",
+  ],
+  [
+    "Wie man gute Guides schreibt",
+    "Den ersten Schritt scheiben",
+    "Den zweiten Schritt schreiben",
+  ],
 ]);
 document.addEventListener("DOMContentLoaded", next);
 
@@ -12,8 +22,10 @@ function next() {
     document.getElementById("step-2").innerHTML = item[2];
   } else {
     document.getElementById("title").innerHTML = "Das waren alle Guides";
+    document.getElementById("title").toggleAttribute("out-of-guides", true);
     document.getElementById("step-1").style.display = "none";
     document.getElementById("step-2").style.display = "none";
+    document.querySelector("button").style.display = "none";
   }
 }
 
