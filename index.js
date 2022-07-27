abfa80f2caffb2257e2a1f44482fd7a7dffa6118
NicholasThierol <nicholasthierolf@gmail.com>
2022-07-28 00:55:03 +0200

    added title

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const content = [
-  ["Einschlafen", "8 Stunden später aufwachen"],
-  ["Einschlafen", "9 Stunden späer aufwachen"],
+  ["Wie man 8 Stunden schläft", "Einschlafen", "8 Stunden später aufwachen"],
+  ["Wie man 9 Stunden schläft", "Einschlafen", "9 Stunden späer aufwachen"],
 ];
 document.addEventListener("DOMContentLoaded", next);
 
 function next() {
   let item = content[Math.floor(Math.random() * content.length)];
-  document.getElementById("step-1").innerHTML = item[0];
-  document.getElementById("step-2").innerHTML = item[1];
+  document.getElementById("title").innerHTML = item[0];
+  document.getElementById("step-1").innerHTML = item[1];
+  document.getElementById("step-2").innerHTML = item[2];
 }
